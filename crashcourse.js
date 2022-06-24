0038e3fea7f886931188bda979324725c78f64c6
Jenny Chau <jnechau@gmail.com>
2022-06-23 22:51:27 -0400

    instructions & programs

--- crashcourse.js
+++ crashcourse.js
@@ -1,5 +1,16 @@
 /*
 
+Crash Course Computer Science
+
+VIII Instructions & Programs
+- CPU is a piece of hardware controlled by easy to modify software
+- Jumps create loops
+- Halts are conditionals that exit the jumps (e.g. jump_negatives, jump_if_equal, jump_if_greater)
+- Modern CPUs have 1) 32 or 64 bits instruction lengths and 2) variable length instructions to create more options for instructions
+- Intel 4004 processor - 46 instructions
+- Modern computers have thousands of different instructions and instruction variants 
+
+
 
 
 */
